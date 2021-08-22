92fc643bc3cc425640d5f1daf571dc3c3d7c50f0
Artem Pavlyuchenko <gwindblaid@gmail.com>
2021-08-22 21:06:12 +0300

    Test1

--- ProblemAcssecGit/Program.cs
+++ ProblemAcssecGit/Program.cs
@@ -6,7 +6,7 @@ namespace ProblemAcssecGit
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! Vadya");
         }
     }
 }
