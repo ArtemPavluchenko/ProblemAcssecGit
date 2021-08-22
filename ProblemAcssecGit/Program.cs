1f0090b6be84c0f8fb9b6a7ee64ad7638ef9ff31
Vadim <bbrb2110@gmail.com>
2021-08-22 21:08:11 +0300

    Test2

--- ProblemAcssecGit/Program.cs
+++ ProblemAcssecGit/Program.cs
@@ -7,6 +7,7 @@ namespace ProblemAcssecGit
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World! Vadya");
+            Console.WriteLine("Hello World! Artem");
         }
     }
 }
